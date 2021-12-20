36db01cdc88cbb19f6dd4bed03894b31093177ec
satyasaigunnam <satyasaigunnam@msitprogram.net>
2021-12-20 20:11:39 +0530

    drawgrid

--- battleship.py
+++ battleship.py
@@ -286,7 +286,7 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    test.testGrid()
+    test.testDrawGrid()
 
     ## Finally, run the simulation to test it manually ##
     runSimulation(500, 500)
