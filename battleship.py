549ef5f13594e368137e27b7db91c2e10ad6e6a8
satyasaigunnam <satyasaigunnam@msitprogram.net>
2021-12-20 19:58:28 +0530

    makeView

--- battleship.py
+++ battleship.py
@@ -288,7 +288,7 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    test.testDrawGrid()
+    test.testGrid()
 
     ## Finally, run the simulation to test it manually ##
     runSimulation(500, 500)
